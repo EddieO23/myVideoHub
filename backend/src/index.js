078fbba1eb7a4f8578576c17274f46e441a2c1dd
Eddie <eocampo52@gmail.com>
2025-05-13 19:29:47 -0500

    Setup between FE and BE
    Added CORS and Axios
    Set up baseURL with Axios

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
 
 
 import routes from './route/index.js'
@@ -10,7 +11,16 @@ const app = express()
 dotenv.config()
 connectDb()
 
+// CORS configuration
+const corsOptions = {
+  origin: ['http://localhost:5173'],
+  optionSuccessStatus: 200 // Replace with your frontend URL
+}
+
+app.use(cors(corsOptions))
 app.use(passportJwtStrategy.initialize()) 
+
+
 
 const port = process.env.PORT || 8000
 
