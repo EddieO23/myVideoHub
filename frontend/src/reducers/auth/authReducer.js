4767843358e8e5fea3681be73275ac1a00b24aa5
Eddie <eocampo52@gmail.com>
2025-05-15 09:24:08 -0500

    Working on signing in the user

--- frontend/src/reducers/auth/authReducer.js
+++ frontend/src/reducers/auth/authReducer.js
@@ -26,10 +26,28 @@ export const signUpUser = createAsyncThunk("auth/sign-up-user", async (payload) 
     } else {
       toast.warning(data.message);
     }
-
-
   } catch (error) {
-    console.error(error);
+    toast.error(error);
+  }
+});
+
+
+// Sign In Request to the backend
+export const signInUser = createAsyncThunk("auth/sign-in-user", async (payload) => { 
+  try {
+    const data = await backendApi.post("/api/v1/auth/sign-in", payload);
+    if (data.success) {
+      if (data.user) {
+        toast.success(data.message);
+        localStorage.setItem("token", data.user.token); // Assuming the token is in data.token
+      }
+      return data.user; // Return the user data to be used in the reducer
+    } else {
+      toast.warning(data.message);
+    }
+  } catch (error) {
+    console.error("Sign-in error:", error); // Log the error
+    toast.error("An error occurred during sign-in."); // Notify the user
   }
 });
 
