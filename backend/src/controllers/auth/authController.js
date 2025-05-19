6aa5829ff38b8f262eea9a4002c0c5d19dfd265c
Eddie <eocampo52@gmail.com>
2025-05-19 11:51:28 -0500

    Backend updatePassword logic
    Added controller and route for updating user password

--- backend/src/controllers/auth/authController.js
+++ backend/src/controllers/auth/authController.js
@@ -25,7 +25,7 @@ export const signUpUser = async (req, res) => {
     });
     // Send repsonse of successful
     return sendResponse(res, 200, true, 'User created successfully.');
-      console.log('Request body:', req.body); // Debugging
+    console.log('Request body:', req.body); // Debugging
     // res.status(200).json({ message: 'User signed up successfully' });
   } catch (error) {
     console.error(`Error in signing up the user ${error}`);
@@ -37,36 +37,58 @@ export const signUpUser = async (req, res) => {
 export const signInUser = async (req, res) => {
   try {
     const { email, password } = req.body;
-    const user = await User.findOne({email})
-    if(!user) {
+    const user = await User.findOne({ email });
+    if (!user) {
       return sendResponse(res, 400, false, 'User does not exist');
     }
     const matchPassword = await comparePassword(password, user.password);
-    if(!matchPassword ) {
+    if (!matchPassword) {
       return sendResponse(res, 400, false, 'Passwords do not match');
     }
     const jwtToken = await generateJwtToken(user);
-    sendResponse(res, 200, true, 'Logged in successfully', {user: {token: jwtToken}});
+    sendResponse(res, 200, true, 'Logged in successfully', {
+      user: { token: jwtToken },
+    });
   } catch (error) {
     console.error(`Error in signing in the user ${error}`);
     return sendResponse(res, 500, false, 'Internal server error');
   }
-}
+};
 
 export const sendEmailForResetPassword = async (req, res) => {
   try {
-    const {email} = req.body
-    if(!email) {
-      return sendResponse(res, 404, false, 'Email not found.')
+    const { email } = req.body;
+    if (!email) {
+      return sendResponse(res, 404, false, 'Email not found.');
     }
-    const user = await User.findOne({email})
-    if(!user) {
-      return sendResponse(res, 404, false, 'User not found.')
+    const user = await User.findOne({ email });
+    if (!user) {
+      return sendResponse(res, 404, false, 'User not found.');
     }
-    await resetPasswordEmail(user)
-    sendResponse(res, 200, true, 'Check your email to reset your password.')
+    await resetPasswordEmail(user);
+    sendResponse(res, 200, true, 'Check your email to reset your password.');
   } catch (error) {
     console.error(`Error in signing in the user ${error}`);
     return sendResponse(res, 500, false, 'Internal server error');
   }
-}
+};
+
+export const updatePassword = async (req, res) => {
+  try {
+    const { token } = req.params;
+    const { password } = req.body;
+    if (!token) {
+      return sendResponse(res, 404, false, 'Token not found.');
+    }
+    const user = await User.findOne({ token });
+    if (!user) {
+      return sendResponse(res, 404, false, 'User not found.');
+    }
+    const hashedPassword = await hashPassword(password);
+    user.password = hashedPassword;
+    await user.save();
+    return sendResponse(res, 200, true, 'Updated your password.');
+  } catch (error) {
+    return sendResponse(res, 500, false, 'Internal server error');
+  }
+};
--- backend/src/route/authRoute.js
+++ backend/src/route/authRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { sendEmailForResetPassword, signInUser, signUpUser } from '../controllers/auth/authController.js'
+import { sendEmailForResetPassword, signInUser, signUpUser, updatePassword } from '../controllers/auth/authController.js'
 
 const router = express.Router()
 
@@ -7,5 +7,6 @@ const router = express.Router()
 router.post('/sign-up', signUpUser)
 router.post('/sign-in', signInUser)
 router.post('/reset-password', sendEmailForResetPassword)
+router.post('/update-password/:token', updatePassword)
 
 export default router
