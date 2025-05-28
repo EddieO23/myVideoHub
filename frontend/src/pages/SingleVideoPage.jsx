d29db4ee8a4331b1eba53c50f372ddf58772cbca
Eddie <eocampo52@gmail.com>
2025-05-28 13:40:36 -0500

    Working on SinglePageVideo frontend

---
+++ frontend/src/pages/SingleVideoPage.jsx
@@ -0,0 +1,13 @@
+import {useParams} from 'react-router-dom'
+
+const SingleVideoPage = () => {
+  const {id} = useParams()
+  const [video, set] 
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SingleVideoPage
