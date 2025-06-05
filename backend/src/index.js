9dc71e451078aaa7cd88b997144dcb107e756cd1
Eddie <eocampo52@gmail.com>
2025-06-05 07:51:53 -0500

    fixed url

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,14 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import cors from 'cors'
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
 
+import routes from './route/index.js';
+import passportJwtStrategy from './config/passportJwtStragetgy.js';
+import connectDb from './config/db.js';
 
-import routes from './route/index.js'
-import passportJwtStrategy from './config/passportJwtStragetgy.js'
-import connectDb from './config/db.js'
-
-const app = express()
-dotenv.config()
-connectDb()
+const app = express();
+dotenv.config();
+connectDb();
 
 // CORS configuration
 // const corsOptions = {
@@ -18,25 +17,28 @@ connectDb()
 // }
 
 const corsOptions = {
-  origin: ['http://localhost:5173', "https://my-video-hub-backend-api-eta.vercel.app"], // Make sure this matches your frontend URL
+  origin: [
+    'http://localhost:5173',
+    'https://my-video-hub-backend-api-eta.vercel.app',
+  ], // Make sure this matches your frontend URL
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
-  optionsSuccessStatus: 200
-}
+  optionsSuccessStatus: 200,
+};
 
-app.use(cors(corsOptions))
-app.use(passportJwtStrategy.initialize()) 
+app.use(cors(corsOptions));
+app.use(passportJwtStrategy.initialize());
 
+const port = process.env.PORT || 8000;
 
+app.get('/', (req, res) => {
+  res.send('Hello world.');
+});
 
-const port = process.env.PORT || 8000
-
-app.get('/', (req, res) => res.send('Hello world.'))
-
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use('/api/v1', routes)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api/v1', routes);
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
-})
+});
